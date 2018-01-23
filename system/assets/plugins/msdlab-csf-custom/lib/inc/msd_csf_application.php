bd0f9297fec932909bd84907bc0eab652bada771
CMO <catherine@msdlab.com>
2018-01-23 15:14:15 -0500

    fix up applicantid on page 2

--- system/assets/plugins/msdlab-csf-custom/lib/inc/msd_csf_application.php
+++ system/assets/plugins/msdlab-csf-custom/lib/inc/msd_csf_application.php
@@ -118,7 +118,7 @@ if (!class_exists('MSDLab_CSF_Application')) {
         function get_applicant_id($user_id){
             global $wpdb;
             $sql = "SELECT ApplicantId FROM applicant WHERE UserId = ". $user_id;
-            //error_log($sql);
+            error_log($sql);
             $result = $wpdb->get_results($sql);
             return $result[0]->ApplicantId;
         }
@@ -185,6 +185,7 @@ if (!class_exists('MSDLab_CSF_Application')) {
                     if ($_POST['application_form']) {
                         //Do the stuff
                         print $this->queries->set_data($form_id . $form_page_number, $set['where']);
+                        if(!$applicant_id){$applicant_id = $this->get_applicant_id($current_user->ID);}
                         if(isset($_POST['SendEmails'])){
                             $this->send_form_emails($_POST['SendEmails']);
                         }
